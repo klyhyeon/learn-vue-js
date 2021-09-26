bed21ecd0b9276e67d53f86db086cdd6dcfece68
yuhyeon <klyhyeon@gmail.com>
2021-09-26 22:32:13 +0900

    feat: 싱글파일 컴포넌트 프로젝트 props, emit

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <app-header 
+    v-bind:propsdata="str"
+    v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
 
 export default {
-  name: 'App',
+  data: function() {
+    return {
+      str: 'Hello world'
+    }
+  },
+  methods: {
+      renewStr: function() {
+        this.str = 'whole new world';
+    }
+  },
   components: {
-    HelloWorld
+    'app-header': AppHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
